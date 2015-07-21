cb1dc9e509e97a67dcc671dd183af979bc9ba01e
Ham <tryu4wk4@gmail.com>
2015-07-21 13:20:39 +0800

    qqq

---
+++ Assets/initChess_normal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class initChess_normal : MonoBehaviour {
+
+	int ChessboardSize=48;
+	int RedWarrior = 3;
+	int RedArcher = 3;
+	int Red = 3;
+	int Red = 10;
+	int BlueWarrior = 3;
+	int BlueWarriorWarrior = 3;
+	int BlueWarriorArcher = 3;
+	int BlueWarrior = 3;
+	int BlueWarrior = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
