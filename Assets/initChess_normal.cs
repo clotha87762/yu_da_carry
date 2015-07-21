b2dd3a62437ef0254078c25742479de38a34dbd0
Ham <tryu4wk4@gmail.com>
2015-07-21 15:28:02 +0800

    init base model

--- Assets/initChess_normal.cs
+++ Assets/initChess_normal.cs
@@ -6,21 +6,34 @@ public class initChess_normal : MonoBehaviour {
 	int ChessboardSize=48;
 	int RedWarrior = 3;
 	int RedArcher = 3;
-	int Red = 3;
-	int Red = 10;
+	int RedWizard = 3;
+	int RedFarmer = 10;
 	int BlueWarrior = 3;
 	int BlueWarriorWarrior = 3;
 	int BlueWarriorArcher = 3;
-	int BlueWarrior = 3;
-	int BlueWarrior = 10;
+	int BlueWizard = 3;
+	int BlueFarmer = 10;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	public void setBoard(){
+		int[] boardCode = new int[ChessboardSize];
+		for (int i=0; i<ChessboardSize; i++) {
+			boardCode[i] = i;
+		}
+		for (int i=0; i<ChessboardSize; i++) {
+			int k = Random.Range(i,47);
+			int temp = boardCode[k];
+			boardCode[k] = boardCode[i];
+			boardCode[i] = temp;
+		}
+		Debug.Log("set board finish");
 	}
 }
--- Assets/initGame.cs
+++ Assets/initGame.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class initGame : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
-	
+		initChess_normal initC = gameObject.GetComponent<initChess_normal> ();
+		initOrder initO = gameObject.GetComponent<initOrder> ();
+
+		initC.setBoard ();
+		initO.setOrder ();
 	}
 	
 	// Update is called once per frame
