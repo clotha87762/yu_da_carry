5f3c0458418116cbfce1b1d9c429b2536c73ae3e
Ham <tryu4wk4@gmail.com>
2015-07-22 17:18:44 +0800

    ratcast, rightClick

---
+++ Assets/raycast.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycast : MonoBehaviour {
+
+	RaycastHit pastHit;
+
+	public gameStateManager GSM;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (GSM.state == 2) {
+			if(GSM.actionState==1|GSM.actionState==2|GSM.actionState==4){
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit ;
+				Physics.Raycast (ray , out hit);
+				
+				//if(GSM.actionState==1&Input.GetMouseButtonDown(1)) click and is pawn , go pawnM
+				//else if(hit == pastHit){}
+				//else{
+					//pastHit = hit
+					//remove
+					//if() pastHit is Grid , go pawnM
+					//else() pastHit is pawn, go pawnM?
+							
+					//light
+					//if()not click and is pawn , go pawnM
+					//else if() not click and is grid , go pawnM
+				//}
+			}
+		}
+		
+	}
+}
---
+++ Assets/rightClickCancel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rightClickCancel : MonoBehaviour {
+
+	public gameStateManager GSM;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GSM.state == 2) {
+			if(Input.GetMouseButtonDown(1)){
+				if(GSM.actionState == 2){
+					//if updated, can't go back
+				}
+				else if(GSM.actionState == 3 ){
+					//close update window
+					//open select window
+					GSM.setActionState(2);
+				}
+				else if(GSM.actionState == 4 ){
+					//open select window
+					GSM.setActionState(2);
+				}
+			}
+		}
+	}
+}
